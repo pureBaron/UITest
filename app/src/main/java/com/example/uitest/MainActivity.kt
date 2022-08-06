db8c481e415c3b05e423b3cf885b719eda2badc1
Baron <amirimox@protonmail.com>
2022-08-06 18:24:00 +0430

    hi

--- app/src/main/java/com/example/uitest/MainActivity.kt
+++ app/src/main/java/com/example/uitest/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.uitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_select_music.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val list : List<String> = listOf("One more light",
+    "The man who sold the world",
+    "one step closer",
+    "sound of silence",
+    "down with the sickness",
+    "glimpse of us",
+    "Skin",
+    "2 step",
+    "A place for my head")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_select_music)
+
+        val layoutManager  = LinearLayoutManager(this)
+        musicRecycle.layoutManager = layoutManager
+
+        val adapter = MusicAdapter(list)
+        musicRecycle.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/uitest/MusicAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.uitest
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MusicAdapter(val list: List<String>) : RecyclerView.Adapter<MusicAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val musicName : TextView = view.findViewById(R.id.musicName)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val name : String = list[position]
+        holder.musicName.text = name
+    }
+
+    override fun getItemCount(): Int {
+        return this.list.size;
+    }
+
+
+}
